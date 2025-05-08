5cbcc191ad6ce7eda27ec3ca9fc30a1c3f9bd20f
Koen van Wijlick <koenvanwijlick@gmail.com>
2025-05-08 15:15:07 +0200

    update on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
+// next.config.mjs
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
+const repo = 'VC_ALAAF'; // GitHub repo name
+
+const config = {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+  output: 'export', // Required for static export
+  images: {
+    unoptimized: true, // Required for export mode
+  },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+};
+
+export default withBundleAnalyzer(config);
